6ecacaa980a7f60baa52f1ee45714445fe3419a5
Srikargoud6 <srikargoud611@gmail.com>
2024-09-24 19:55:23 +0530

    added middleware to authenticate user to get cart details from db

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import { connectDb } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
+import cartRouter from "./routes/cartRoute.js";
 import "dotenv/config";
 
 //app config
@@ -20,6 +21,7 @@ connectDb();
 app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
 
 app.get("/", (req, res) => {
   res.send("API working...");
