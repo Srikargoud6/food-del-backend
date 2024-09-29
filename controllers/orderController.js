03acd68fa726f35828fa703d6dd606fcf615f510
Srikargoud6 <srikargoud611@gmail.com>
2024-09-29 16:11:46 +0530

    remove few items from menu list

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,8 +1,8 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
-import Stripe from "stripe";
+// import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 //placing user order from frontend
 
@@ -18,35 +18,35 @@ const placeOrder = async (req, res) => {
     await newOrder.save();
     await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
 
-    const line_items = req.body.items.map((item) => ({
-      price_data: {
-        currency: "usd",
-        product_data: {
-          name: item.name,
-        },
-        unit_amount: item.price,
-        quantity: item.quantity,
-      },
-    }));
+    // const line_items = req.body.items.map((item) => ({
+    //   price_data: {
+    //     currency: "usd",
+    //     product_data: {
+    //       name: item.name,
+    //     },
+    //     unit_amount: item.price,
+    //     quantity: item.quantity,
+    //   },
+    // }));
 
-    line_items.push({
-      price_data: {
-        currency: "usd",
-        product_data: {
-          name: "Delivery Charges",
-        },
-        unit_amount: 2 * 100,
-      },
-      quantity: 1,
-    });
+    // line_items.push({
+    //   price_data: {
+    //     currency: "usd",
+    //     product_data: {
+    //       name: "Delivery Charges",
+    //     },
+    //     unit_amount: 2 * 100,
+    //   },
+    //   quantity: 1,
+    // });
 
-    const session = await stripe.checkout.sessions.create({
-      line_items: line_items,
-      mode: "payment",
-      success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-      cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
-    });
-    res.json({ success: true, success_url: session.url });
+    // const session = await stripe.checkout.sessions.create({
+    //   line_items: line_items,
+    //   mode: "payment",
+    //   success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+    //   cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+    // });
+    res.json({ success: true, message: "order Placed" });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: "Error" });
